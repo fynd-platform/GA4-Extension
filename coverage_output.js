8eecb5245640437d33490f6c573b439971d4f69c
Abhishek Raikwar <abhishekraikwar@gofynd.com>
2024-06-20 15:22:35 +0530

    Cleanup

--- coverage_output.js
+++ coverage_output.js
@@ -17,16 +17,6 @@ let webData = fs.readFileSync(
 data = JSON.parse(data).total;
 webData = JSON.parse(webData).total;
 
-// let output = data;
-// let output = {
-//     coverage_pct: (data.lines.pct + webData.lines.pct)/2,
-//     lines_total: data.lines.total + webData.lines.total,
-//     lines_covered: data.lines.covered + webData.lines.covered,
-//     branch_pct: (data.branches.pct + webData.branches.pct)/2,
-//     branches_covered: data.branches.covered + webData.branches.covered,
-//     branches_total: data.branches.total + webData.branches.total,
-// }
-
 let output = {
   coverage_pct: data.lines.pct,
   lines_total: data.lines.total,
@@ -35,15 +25,6 @@ let output = {
   branches_covered: data.branches.covered + data.branches.covered,
   branches_total: data.branches.total + data.branches.total,
 };
-
-// let output = {
-//   coverage_pct: webData.lines.pct,
-//   lines_total: webData.lines.total,
-//   lines_covered: webData.lines.covered,
-//   branch_pct: (data.branches.pct + webData.branches.pct) / 2,
-//   branches_covered: data.branches.covered + webData.branches.covered,
-//   branches_total: data.branches.total + webData.branches.total,
-// };
 
 console.log('Coverage Output : ', output);
 output = JSON.stringify(output, null, 2);
